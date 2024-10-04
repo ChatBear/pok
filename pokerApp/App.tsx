39690bbb6a8c9d728baf0104a5675027523db58f
ChatBear <adamaly.shiraz@outlook.fr>
2024-10-04 23:00:28 +0700

    first commit

---
+++ pokerApp/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text> 
+        Bienvenue dans le monde du poker 
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+});
+
+export default App;
